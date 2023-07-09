3e95ba1cb64508367f066a1a88f68daafe1b976e
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-07-09 16:47:48 +0100

    Chore(): project has been setup

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express, { Application, Request, Response } from 'express';
+import log from './logger/customlogger';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+export const app: Application = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+// app.use('/api', routes);
+
+app.get('/', (req: Request, res: Response) => {
+	res.status(200).json({
+		message:
+            'Welcome to Neo-Quotations \n get the best deals',
+	});
+	log.info('BOOM 🔥🔥');
+});
+
+app.get('*', (req: Request, res: Response) => {
+	res.status(404).json({ message: 'This route does not exist' });
+});
